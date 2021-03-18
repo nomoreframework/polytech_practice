da387ec55381ce3968c9221e7a9b1505b4d23ab4
nomoreframework <gleblukianenko@gmail.com>
2021-03-18 15:09:37 +0300

    added new empty console project for the next lesson

---
+++ Practice_2/matrix_type/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace matrix_type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
